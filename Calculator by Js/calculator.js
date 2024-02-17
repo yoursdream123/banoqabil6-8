39cf645327439fe335f41ac829ce3d4859ef5f81
yoursdream123 <153999067+yoursdream123@users.noreply.github.com>
2024-02-17 23:01:11 +0500

    Add files via upload

---
+++ Calculator by Js/calculator.js
@@ -0,0 +1,35 @@
+
+// -----------------------Addition---------------------
+function add(){
+    let a = parseInt(prompt(`Enter First Number `))
+    let b = parseInt(prompt(`Enter Second Number `))
+    let c = a + b;
+    alert(`The Addition of ${a} and ${b} is: ${c}`)
+}
+
+
+// ------------------------Subtraction---------------------
+function sub(){
+    let d = parseInt(prompt(`Enter First Number `))
+    let e = parseInt(prompt(`Enter Second Number `))
+    let f = d - e;
+    alert(`The Subtraction of ${d} and ${e} is: ${f}`)
+}
+
+
+// --------------------------Multiplication------------------
+function multi(){
+    let g = parseInt(prompt(`Enter First Number `))
+    let h = parseInt(prompt(`Enter Second Number `))
+    let j = g * h;
+    alert(`The Multiplication of ${g} and ${h} is: ${j}`)
+}
+
+
+// ---------------------------Division-------------------------
+function divi(){
+    let k = parseInt(prompt(`Enter First Number `))
+    let l = parseInt(prompt(`Enter Second Number `))
+    let m = k / l;
+    alert(`The Division of ${k} and ${l} is: ${m}`)
+}
